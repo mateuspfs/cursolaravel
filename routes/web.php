d2a23e757fcbebb387b89dbcfc6d06736fb23dad
Mateus Sampaio <mateuspfs3@gmail.com>
2023-10-09 00:10:15 -0300

    config routes

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/empresa', function(){
-    return view('site/empresa');
+    return redirect()->route('admin.dashboard');
 });
 
 Route::any('/any', function(){
@@ -29,6 +25,56 @@ Route::match(['put', 'delete'], '/match', function(){
     return 'Permite apenas acessos definidos';
 });
 
+// concatenando variavéis junto da rota 
 Route::get('/produto/{id}/{cat}', function($id, $cat){
     return 'O id do produto é: ' . $id . '<br> A categoria é: ' . $cat;
 });
+
+// caminho facilitado
+Route::redirect('/sobre', '/empresa');
+Route::view('/empresa', 'site/empresa');
+
+// adicionando nome a esta rota
+Route::get('/timesnownews', function(){
+    return view('news');
+})->name('noticias');
+
+// redireciona a outra rota através do nome dado a ela 
+Route::get('/novidades', function(){
+    return redirect()->route('noticias');
+});
+
+// sem usar prefix ou name
+// Route::get('/admin/users', function(){
+//     return 'users';
+// })->name('admin.users');
+
+// Route::get('/admin/clientes', function(){
+//     return 'clientes';
+// })->name('admin.clientes');
+
+// Route::get('/admin/dashboard', function(){
+//     return 'dashboard';
+// })->name('admin.clientes');
+
+// agrupando por prefix e nome
+
+
+Route::group([
+    'prefix' => 'admin',
+    'as' => 'admin.'
+], function(){
+
+    Route::get('users', function(){
+        return "users";
+    })->name('users');
+    
+    Route::get('clientes', function(){
+        return "clientes";
+    })->name('clientes');
+    
+    Route::get('dashboard', function(){
+        return "dashboard";
+    })->name('dashboard');
+});
+
