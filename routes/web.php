2263f8b5bd245db4806da8c6ef793b7dc80412f3
Mateus Sampaio <mateuspfs3@gmail.com>
2023-10-26 19:53:47 -0300

    controllers and migrates

--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,40 @@
 <?php
 
+use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Routes 
 
-Route::get('/', function () {
-    return redirect()->route('admin.dashboard');
-});
+// Route::get('/', function () {
+//     return redirect()->route('admin.dashboard');
+// });
 
-Route::any('/any', function(){
-    return 'Permite todo tipo de acesso http(put, delete, get and post)';
-});
+// Route::any('/any', function(){
+//     return 'Permite todo tipo de acesso http(put, delete, get and post)';
+// });
 
-Route::match(['put', 'delete'], '/match', function(){
-    return 'Permite apenas acessos definidos';
-});
+// Route::match(['put', 'delete'], '/match', function(){
+//     return 'Permite apenas acessos definidos';
+// });
 
-// concatenando variavéis junto da rota 
-Route::get('/produto/{id}/{cat}', function($id, $cat){
-    return 'O id do produto é: ' . $id . '<br> A categoria é: ' . $cat;
-});
+// // concatenando variavéis junto da rota 
+// Route::get('/produto/{id}/{cat}', function($id, $cat){
+//     return 'O id do produto é: ' . $id . '<br> A categoria é: ' . $cat;
+// });
 
-// caminho facilitado
-Route::redirect('/sobre', '/empresa');
-Route::view('/empresa', 'site/empresa');
+// // caminho facilitado
+// Route::redirect('/sobre', '/empresa');
+// Route::view('/empresa', 'site/empresa');
 
-// adicionando nome a esta rota
-Route::get('/timesnownews', function(){
-    return view('news');
-})->name('noticias');
+// // adicionando nome a esta rota
+// Route::get('/timesnownews', function(){
+//     return view('news');
+// })->name('noticias');
 
-// redireciona a outra rota através do nome dado a ela 
-Route::get('/novidades', function(){
-    return redirect()->route('noticias');
-});
+// // redireciona a outra rota através do nome dado a ela 
+// Route::get('/novidades', function(){
+//     return redirect()->route('noticias');
+// });
 
 // sem usar prefix ou name
 // Route::get('/admin/users', function(){
@@ -57,24 +49,26 @@ Route::get('/novidades', function(){
 //     return 'dashboard';
 // })->name('admin.clientes');
 
+
 // agrupando por prefix e nome
+// Route::group([
+//     'prefix' => 'admin',
+//     'as' => 'admin.'
+// ], function(){
 
-
-Route::group([
-    'prefix' => 'admin',
-    'as' => 'admin.'
-], function(){
-
-    Route::get('users', function(){
-        return "users";
-    })->name('users');
+//     Route::get('users', function(){
+//         return "users";
+//     })->name('users');
     
-    Route::get('clientes', function(){
-        return "clientes";
-    })->name('clientes');
+//     Route::get('clientes', function(){
+//         return "clientes";
+//     })->name('clientes');
     
-    Route::get('dashboard', function(){
-        return "dashboard";
-    })->name('dashboard');
-});
+//     Route::get('dashboard', function(){
+//         return "dashboard";
+//     })->name('dashboard');
+// });
 
+// Controllers
+
+Route::resource('produtos', ProdutoController::class);
